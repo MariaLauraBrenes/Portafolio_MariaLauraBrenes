9579e8a8bcfadca06caaa4b3e868d7e632e77a41
MariaLauraBrenes <mbrenes20078@ufide.ac.cr>
2024-12-03 07:15:18 -0600

    Semana 11

--- TiendaLauraBrenes/src/main/java/TiendaMariaLauraBrenes/demo/dao/UsuarioDao.java
+++ TiendaLauraBrenes/src/main/java/TiendaMariaLauraBrenes/demo/dao/UsuarioDao.java
@@ -8,5 +8,11 @@ import TiendaMariaLauraBrenes.demo.domain.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsuarioDao extends JpaRepository<Usuario, Long> {
-Usuario findByUsername (String username);   
+    Usuario findByUsername(String username);
+    
+    Usuario findByUsernameAndPassword(String username, String Password);
+
+    Usuario findByUsernameOrCorreo(String username, String correo);
+
+    boolean existsByUsernameOrCorreo(String username, String correo);
 }
